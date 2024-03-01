782708afb266d4f35ddfee3e4b85cd46ebe1bcaf
Pedro Falco <fpfalco@gmail.com>
2024-03-01 10:03:42 -0300

    removing use enhance

--- src/lib/components/Subscribe.svelte
+++ src/lib/components/Subscribe.svelte
@@ -4,7 +4,7 @@
     import { onMount } from 'svelte';
     import { indexStyleStore } from '$lib/store.js';
 
-    export const prerender = true;
+    // export const prerender = true;
 
     let index = 0;
 
@@ -18,7 +18,7 @@
 
 </script>
 
-<form method="POST" netlify netlify-honeypot="bot-field" use:enhance class="bg-primary text-secondary flex items-center justify-between gap-4 p-4 rounded-lg mt-8 lp:mt-24">
+<form method="POST" class="bg-primary text-secondary flex items-center justify-between gap-4 p-4 rounded-lg mt-8 lp:mt-24">
     <label for="email" class="self-start font-medium text-xs lp:text-sm my-auto tracking-wider">Subscribe</label>
     <input type="email" name="EMAIL" class="invalid bg-transparent font-normal flex-grow text-xs lp:text-base border-secondary border-b-[1px] outline-none" required value="">
     <button type="submit" class="outline-none flex-none"><img src={`/arrow-subscribe-${estilos[index].secondary_media}.svg`} alt="Subscribe"></button>
