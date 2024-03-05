5ce1d0f17f1b1eaa6dba71c3f65839334b15529f
Pedro Falco <fpfalco@gmail.com>
2024-03-05 20:38:52 -0300

    new deploy without await

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,4 @@
 import { API_ENDPOINT } from '$env/static/private';
-import fetch from 'node-fetch';
 
 export const actions = {
     default: async ({ request }) => {
@@ -7,23 +6,17 @@ export const actions = {
       const email = formData.get('EMAIL'); 
       
     //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
-      const response = await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
+      fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
         // body: new URLSearchParams({ EMAIL: email }),
         body: new URLSearchParams({ 'EMAIL': email }),
-      });
-  
-      if (response.ok) {
-        console.log('Success');
-        return { success: true };
-    } else {
-          console.log('Error');
-          console.log(await response.text());
-        return { success: false, error: await response.text() };
-      }
-    },
-  };
-  
+      }).then((response) => {
+        console.log('success');
+    }).catch((error) => {
+        console.log('error');
+    });
+  }
+};
