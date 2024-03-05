4a213cfce624592576d92a06f12a7d8a3101e015
Pedro Falco <fpfalco@gmail.com>
2024-03-05 09:56:23 -0300

    pushin to vercel

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,7 +5,8 @@ export const actions = {
       const formData = await request.formData();
       const email = formData.get('EMAIL'); 
       
-      const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
+    //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
+      const response = await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -19,6 +20,7 @@ export const actions = {
         return { success: true };
     } else {
           console.log('Error');
+          console.log(await response.text());
         return { success: false, error: await response.text() };
       }
     },
