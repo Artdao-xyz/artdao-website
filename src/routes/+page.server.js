a6a5cd5ae09161f7a53d2510f5ce2522161138bc
Pedro Falco <fpfalco@gmail.com>
2024-03-06 00:32:32 -0300

    removing use enhance

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -30,13 +30,11 @@ export const actions = {
         const formData = await request.formData();
         const email = formData.get('EMAIL'); 
         console.log('email', email);
-    //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
         let response = await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
-        // body: new URLSearchParams({ EMAIL: email }),
         body: new URLSearchParams({ 'EMAIL': email }),
       })
 
