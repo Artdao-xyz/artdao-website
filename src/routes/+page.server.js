92a8fa5bf7cd849328f68ab2cc5a4d79f02b7d1b
Pedro Falco <fpfalco@gmail.com>
2024-03-05 22:09:53 -0300

    latest version

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -27,9 +27,9 @@
 
 export const actions = {
     default: async ({ request }) => {
-      const formData = 'fromtheserver@gmail.com';
-    //   const email = formData.get('EMAIL'); 
-    // console.log('email', email);
+        const formData = await request.formData();
+        const email = formData.get('EMAIL'); 
+        console.log('email', email);
     //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
       fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
         method: 'POST',
@@ -37,12 +37,13 @@ export const actions = {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
         // body: new URLSearchParams({ EMAIL: email }),
-        body: new URLSearchParams({ 'EMAIL': formData }),
+        body: new URLSearchParams({ 'EMAIL': email }),
       }).then((response) => {
-        console.log('response', response.data);
-        console.log(JSON.stringify(response.data));
+        console.log('response', response.status);
+        return response.status;
     }).catch((error) => {
-        console.log(error);
+        console.error(error);
+        return response.status;
     });
   }
 };
