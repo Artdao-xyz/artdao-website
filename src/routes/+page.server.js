919744c15782ab6284faaa1a9d17f20fad4127fa
Pedro Falco <fpfalco@gmail.com>
2024-03-05 21:52:53 -0300

    hardcoding the email

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -27,9 +27,9 @@
 
 export const actions = {
     default: async ({ request }) => {
-      const formData = await request.formData();
-      const email = formData.get('EMAIL'); 
-    console.log('email', email);
+      const formData = 'fromtheserver@gmail.com';
+    //   const email = formData.get('EMAIL'); 
+    // console.log('email', email);
     //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
       fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
         method: 'POST',
@@ -37,7 +37,7 @@ export const actions = {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
         // body: new URLSearchParams({ EMAIL: email }),
-        body: new URLSearchParams({ 'EMAIL': email }),
+        body: new URLSearchParams({ 'EMAIL': formData }),
       }).then((response) => {
         console.log('response', response.data);
         console.log(JSON.stringify(response.data));
