da6b2c9a3ca79c4063813e81f840dcfad5ac7e15
Pedro Falco <fpfalco@gmail.com>
2024-03-05 23:37:29 -0300

    adding custom enhancing

--- src/lib/components/Subscribe.svelte
+++ src/lib/components/Subscribe.svelte
@@ -18,7 +18,17 @@
 
 </script>
 
-<form method="POST" name="email" use:enhance action="https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0" class="bg-primary text-secondary flex items-center justify-between gap-4 p-4 rounded-lg mt-8 lp:mt-24">
+<form method="POST" name="email" use:enhance={({form, data, cancel}) => {
+    return ({ result }) => {
+        if (result.type === 'success') {
+            form.reset();
+            alert('Thank you for subscribing!');
+        } else {
+            alert('There was an error subscribing. Please try again.');
+        }
+    };
+}} 
+    action="https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0" class="bg-primary text-secondary flex items-center justify-between gap-4 p-4 rounded-lg mt-8 lp:mt-24">
     <label for="email" class="self-start font-medium text-xs lp:text-sm my-auto tracking-wider">Subscribe</label>
     <input type="email" name="EMAIL" class="invalid bg-transparent font-normal flex-grow text-xs lp:text-base border-secondary border-b-[1px] outline-none" required value="">
     <button type="submit" class="outline-none flex-none"><img src={`/arrow-subscribe-${estilos[index].secondary_media}.svg`} alt="Subscribe"></button>
