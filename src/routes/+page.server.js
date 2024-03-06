833e56ec10dfce097f9f3954f6dafe03560c0785
Pedro Falco <fpfalco@gmail.com>
2024-03-05 22:36:30 -0300

    removing await on fetch function

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -31,7 +31,7 @@ export const actions = {
         const email = formData.get('EMAIL'); 
         console.log('email', email);
     //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
-        await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
+        fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
