0a9a782b2ce5c407da192007235f9404c2a74395
Pedro Falco <fpfalco@gmail.com>
2024-03-06 00:16:00 -0300

    adding await

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -31,19 +31,16 @@ export const actions = {
         const email = formData.get('EMAIL'); 
         console.log('email', email);
     //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
-        await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
+        let response = await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
         // body: new URLSearchParams({ EMAIL: email }),
         body: new URLSearchParams({ 'EMAIL': email }),
-      }).then((response) => {
-        console.log('response', response.status);
-        return response.status;
-    }).catch((error) => {
-        console.error(error);
-        return response.status;
-    });
+      })
+
+      // await and console response
+        console.log(await response.text());
   }
 };
