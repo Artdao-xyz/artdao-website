44b4a538c0574ddd20ce960e7ea65cd5c2739b31
Pedro Falco <fpfalco@gmail.com>
2024-03-05 21:23:02 -0300

    changing to axios

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,25 +1,47 @@
-import { API_ENDPOINT } from '$env/static/private';
-
-import fetch from 'node-fetch';
+// import { API_ENDPOINT } from '$env/static/private';
+import axios from 'axios';
 
 export const actions = {
     default: async ({ request }) => {
-      const formData = 'fromtheserver@gmail.com';
-    //   const email = formData.get('EMAIL'); 
-    // console.log('email', email);
-    //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
-      fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        // body: new URLSearchParams({ EMAIL: email }),
-        body: new URLSearchParams({ 'EMAIL': formData }),
-      }).then((response) => {
-        console.log('response', response.data);
-        console.log(JSON.stringify(response.data));
-    }).catch((error) => {
-        console.log(error);
-    });
-  }
+        const formData = await request.formData();
+        const email = formData.get('EMAIL'); 
+
+        var config = {
+            method: 'POST',
+            url: 'https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: email
+        };
+
+        await axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 };
+// export const actions = {
+//     default: async ({ request }) => {
+//       const formData = await request.formData();
+//       const email = formData.get('EMAIL'); 
+//     console.log('email', email);
+//     //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
+//       fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//         // body: new URLSearchParams({ EMAIL: email }),
+//         body: new URLSearchParams({ 'EMAIL': email }),
+//       }).then((response) => {
+//         console.log('response', response.data);
+//         console.log(JSON.stringify(response.data));
+//     }).catch((error) => {
+//         console.log(error);
+//     });
+//   }
+// };
