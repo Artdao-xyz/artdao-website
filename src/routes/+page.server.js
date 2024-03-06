fc4b4d39984e1e56970b462bcb496114c56bc66d
Calvin Hoenes <calvinhoenes@gmail.com>
2024-03-06 14:37:38 +1100

    chore: mailchimp form submission return

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,18 +3,21 @@ export const actions = {
         const formData = await request.formData();
         const email = formData.get('EMAIL'); 
         console.log('email', email);
+        let body = new URLSearchParams();
+        body.append('EMAIL', email);
         let response = await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
           },
           // body: new URLSearchParams({ EMAIL: email }),
-          body: new URLSearchParams({ 'EMAIL': email }),
+          body
         })
 
-        console.log('response', response);
-
-        // await and console response
-        console.log(await response.text());
+        if(response.status === 200) {
+          return { success: true }
+        } else {
+          return { success: false, error: 'There was an error: ' + (await response.text())}
+        }
   }
 };
