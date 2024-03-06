a50e686d57ceac6c1d13ffb1b35d2a7bb0f5a187
Calvin Hoenes <calvinhoenes@gmail.com>
2024-03-06 14:23:51 +1100

    feat: test

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,46 +1,20 @@
-// import { API_ENDPOINT } from '$env/static/private';
-// import axios from 'axios';
-
-// export const actions = {
-//     default: async ({ request }) => {
-//         const formData = await request.formData();
-//         const email = formData.get('EMAIL'); 
-
-//         var config = {
-//             method: 'POST',
-//             url: 'https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             data: email
-//         };
-
-//         await axios(config)
-//             .then(function (response) {
-//                 console.log(JSON.stringify(response.data));
-//             })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     }
-// };
-
 export const actions = {
     default: async ({ request }) => {
         const formData = await request.formData();
         const email = formData.get('EMAIL'); 
         console.log('email', email);
-    //   const response = await fetch(process.env.API_ENDPOINT || API_ENDPOINT, {
         let response = await fetch('https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        // body: new URLSearchParams({ EMAIL: email }),
-        body: new URLSearchParams({ 'EMAIL': email }),
-      })
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          // body: new URLSearchParams({ EMAIL: email }),
+          body: new URLSearchParams({ 'EMAIL': email }),
+        })
 
-      // await and console response
+        console.log('response', response);
+
+        // await and console response
         console.log(await response.text());
   }
 };
