a9f08f8d4afde4092853299cd2afd2c373e0eb2d
Calvin Hoenes <calvinhoenes@gmail.com>
2024-03-06 15:00:25 +1100

    chore: switched to mailchimp api endpoint

--- src/routes/api/subscribe/+server.ts
+++ src/routes/api/subscribe/+server.ts
@@ -1,28 +1,32 @@
 import { json } from '@sveltejs/kit';
+import { MAILCHIMP_API_KEY, MAILCHIMP_LIST_ID } from '$env/static/private';
+
+const API_KEY = MAILCHIMP_API_KEY;
+const LIST_ID = MAILCHIMP_LIST_ID;
+const SERVER_PREFIX = 'usX'; // Replace 'usX' with your actual data center prefix
+const URL = `https://${SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`;
 
 export async function POST({ request }) {
 	const { email } = await request.json();
 
-    console.log('email', email);
-    let body = new URLSearchParams();
-    body.append('EMAIL', email);
-    let response = await fetch(
-        'https://xyz.us11.list-manage.com/subscribe/post?u=d150dd71762335c56d7e5811c&id=6f099dd01d&f_id=005aaae0f0',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            // body: new URLSearchParams({ EMAIL: email }),
-            body
-        }
-    );
+	let body = JSON.stringify({
+		email_address: email,
+		status: 'subscribed' // Use 'pending' to send a confirmation email
+	});
 
-    console.log('response', response);
+	let response = await fetch(URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + API_KEY
+		},
+		body
+	});
 
-    if (response.status === 200) {
-        return json({ success: true });
-    } else {
-        return json({ success: false, error: 'There was an error: ' + (await response.text()) });
-    }
+	if (response.ok) {
+		return json({ success: true });
+	} else {
+		const errorText = await response.text();
+		return json({ success: false, error: 'There was an error: ' + errorText });
+	}
 }
