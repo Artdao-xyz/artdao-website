c570ca133c40a58ce7f1344a92c4fb7d4eaac342
Calvin Hoenes <calvinhoenes@gmail.com>
2024-03-06 14:39:38 +1100

    chore: added enhance logs

--- src/lib/components/Subscribe.svelte
+++ src/lib/components/Subscribe.svelte
@@ -18,7 +18,15 @@
 
 </script>
 
-<form method="POST" use:enhance class="bg-primary text-secondary flex items-center justify-between gap-4 p-4 rounded-lg mt-8 lp:mt-24">
+<form method="POST" 
+    use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+		console.log('submitting form with: ', formData);
+		return async ({ result, update }) => {
+            console.log('result', result);
+		};
+	}}
+    class="bg-primary text-secondary flex items-center justify-between gap-4 p-4 rounded-lg mt-8 lp:mt-24"
+>
     <label for="email" class="self-start font-medium text-xs lp:text-sm my-auto tracking-wider">Subscribe</label>
     <input type="email" name="EMAIL" class="invalid bg-transparent font-normal flex-grow text-xs lp:text-base border-secondary border-b-[1px] outline-none" required value="">
     <button type="submit" class="outline-none flex-none"><img src={`/arrow-subscribe-${estilos[index].secondary_media}.svg`} alt="Subscribe"></button>
