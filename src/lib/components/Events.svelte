d6a3779c1d34cb3dd66b92aaf18cc06fcba1b7ed
tkdevolution <tkdevolution.app@gmail.com>
2024-04-03 13:23:30 -0300

    chore: fix some bugs on events

--- src/lib/components/Events.svelte
+++ src/lib/components/Events.svelte
@@ -20,7 +20,7 @@
 			what: 'Psipsikoko',
 			where: 'Buenos Aires',
 			info: 'exhibition',
-			address: 'OCTOBER 26th-27th/2023 AURA FABRICA, BUENOS AIRES'
+			address: 'OCTOBER 26th-27th/2023 - AURA FABRICA, BUENOS AIRES'
 		},
 		{
 			banner: [
@@ -31,7 +31,7 @@
 			what: 'Non\\Places',
 			where: 'Paris',
 			info: 'exhibition/party',
-			address: 'JULY 18th-20th/2023 31 RUE HENRI CHEVREU, PARIS'
+			address: 'JULY 18th-20th/2023 - 31 RUE HENRI CHEVREU, PARIS'
 		},
 		{
 			banner: [
@@ -42,7 +42,7 @@
 			what: 'Intertwined',
 			where: 'NFC Lisbon',
 			info: 'exhibition/panels/After Party',
-			address: '06-08/06/2023 TV. DO MOINHO de vento 24lb'
+			address: 'JUNE 6th-8th/2023 - Armazém souL, lisbon'
 		},
 		{
 			banner: [
@@ -54,7 +54,7 @@
 			what: 'Cero uno',
 			where: 'Buenos Aires',
 			info: 'exhibition/talks/After Party',
-			address: 'MARCH 31th/2023 ARTLAB, BUENOS AIRES'
+			address: 'MARCH 31th/2023 - ARTLAB, BUENOS AIRES'
 		},
 		{
 			banner: [
@@ -65,7 +65,7 @@
 			what: 'ETH Denver',
 			where: 'Denver',
 			info: 'exhibition/workshops/AFTER party',
-			address: 'MARCH 2th - 6th /2023 DENVER, COLORADO'
+			address: 'MARCH 2th - 6th /2023 - DENVER, COLORADO'
 		},
 		{
 			banner: [
@@ -76,7 +76,7 @@
 			what: 'DEVCON',
 			where: 'Bogotá',
 			info: 'exhibition/mural/talks',
-			address: 'OCTOBER 11th-14th/2022 AGORA BOGOTÁ CONVENTION CENTER, BOGOTÁ'
+			address: 'OCTOBER 11th-14th/2022 - AGORA BOGOTÁ CONVENTION CENTER, BOGOTÁ'
 		},
 		{
 			banner: [
@@ -86,7 +86,7 @@
 			what: 'NFT Paris',
 			where: 'Paris',
 			info: 'Lukas Truniger Showcase',
-			address: '101 Rue Réaumur75002, Paris'
+			address: '101 Rue Réaumur 75002, Paris'
 		},
 		{
 			banner: [
@@ -99,6 +99,18 @@
 			where: 'São Paulo',
 			info: 'Exhibition',
 			address: 'MARCH 23th - APRIL 6th /2024 SÃO PAULO, BRAZIL'
+		},
+		{
+			banner: [
+				'/events/underworld/underworld-1.png',
+				'/events/underworld/underworld-2.png',
+				'/events/underworld/underworld-3.png',
+				'/events/underworld/underworld-4.png'
+			],
+			what: 'Underworld',
+			where: 'Lisbon',
+			info: 'Exhibition/Party',
+			address: 'JULY 18th-20th/2023 31 RUE HENRI CHEVREU, PARIS'
 		}
 	];
 
@@ -135,6 +147,8 @@
 		isParisOpen = false;
 		isBotogaOpen = false;
 		isDenverOpen = false;
+		isSaoPauloOpen = false;
+
 	};
 	const openBuenosAires = () => {
 		isBuenosAiresOpen = isBuenosAiresOpen ? false : true;
@@ -142,6 +156,7 @@
 		isParisOpen = false;
 		isBotogaOpen = false;
 		isDenverOpen = false;
+		isSaoPauloOpen = false;
 	};
 	const openParis = () => {
 		isParisOpen = isParisOpen ? false : true;
@@ -149,6 +164,7 @@
 		isLisboaOpen = false;
 		isBotogaOpen = false;
 		isDenverOpen = false;
+		isSaoPauloOpen = false;
 	};
 
 	const openBogota = () => {
@@ -157,6 +173,7 @@
 		isLisboaOpen = false;
 		isParisOpen = false;
 		isDenverOpen = false;
+		isSaoPauloOpen = false;
 	};
 
 	const openDenver = () => {
@@ -165,6 +182,7 @@
 		isLisboaOpen = false;
 		isParisOpen = false;
 		isBotogaOpen = false;
+		isSaoPauloOpen = false;
 	};
 
     const openSaoPaulo = () => {
@@ -183,6 +201,7 @@
 		isLisboaOpen = false;
 		isBotogaOpen = false;
 		isDenverOpen = false;
+		isSaoPauloOpen = false;
 	};
 
 	const closeEvent = () => {
@@ -214,7 +233,7 @@
 	<div class="h-[80%] lp:h-auto w-fit mx-auto flex flex-col justify-center items-center gap-8">
 		<a
 			href={'#'}
-			class="relative lp:absolute bg-primary text-secondary lp:left-[25%] dp:left-[25%] lp:top-[45%] sp:top-[32%] dp:top-[40%] bg:top-[35%] bg:left-[25%] 2k:top-[38%] 2k:left-[25%] px-3 py-1 rounded-sm shadow-md gap-3"
+			class="relative lp:absolute bg-primary text-secondary lp:left-[25%] lp:top-[35%] dp:left-[25%] lp:top-[45%] sp:top-[32%] dp:top-[35%] bg:top-[35%] bg:left-[25%] 2k:top-[38%] 2k:left-[25%] px-3 py-1 rounded-sm shadow-md gap-3"
 		>
 			<a on:click={openDenver} href={'#'}>
 				<p class="normal-case text-xl font-medium font-neue-power tracking-wide">Denver</p>
@@ -238,7 +257,7 @@
 
 		<a
 			href={'#'}
-			class="relative lp:absolute bg-primary text-secondary lp:left-[27.5%] lp:top-[65%] sp:top-[60%] bg:top-[60%] bg:left-[30%] 2k:left-[28%] px-3 py-1.5 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
+			class="relative lp:absolute bg-primary text-secondary lp:left-[30%] lp:top-[65%] sp:top-[65%] sp:left-[30%]  bg:top-[60%] bg:left-[30%] 2k:left-[30%] px-3 py-1.5 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
 		>
 			<a on:click={openBuenosAires} href={'#'}>
 				<p class="normal-case text-xl font-medium font-neue-power tracking-wide">Buenos Aires</p>
@@ -274,8 +293,32 @@
 		</a>
 
 		<a
+		href={'#'}
+		class="relative lp:absolute bg-primary text-secondary lp:left-[32%] lp:top-[57%] sp:top-[57%] dp:top-[57%] dp:left-[32%] bg:top-[54%] bg:left-[32%] 2k:top-[54%] 2k:left-[32%] px-3 py-1 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
+	>
+		<a on:click={openSaoPaulo} href={'#'}>
+			<p class="normal-case text-xl font-medium font-neue-power tracking-wide">São Paulo</p>
+		</a>
+		<div class={`${isSaoPauloOpen ? 'block' : 'hidden'} h-full`}>
+			<hr class="border-secondary mt-2" />
+			<a
+				on:click={() => pickEvent(7)}
+				href={'#'}
+				class="flex justify-between items-center py-2 gap-36"
+			>
+				<p class="text-sm font-normal tracking-wide">O.R.B</p>
+				<img
+					loading="lazy"
+					src={`/events/events-pick-${estilos[index].secondary_media}.svg`}
+					alt="pick event"
+				/>
+			</a>
+		</div>
+	</a>
+
+		<a
 			href={'#'}
-			class="relative lp:absolute bg-primary text-secondary lp:left-[30%] lp:top-[55%] sp:left-[27%] sp:top-[45%] dp:left-[30%] dp:top-[50%] bg:top-[48%] bg:left-[28%] 2k:top-[48%] 2k:left-[28%] px-3 py-1 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
+			class="relative lp:absolute bg-primary text-secondary lp:left-[28%] lp:top-[49%] sp:left-[27%] sp:top-[45%] dp:left-[30%] dp:top-[50%] bg:top-[48%] bg:left-[28%] 2k:top-[48%] 2k:left-[28%] px-3 py-1 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
 		>
 			<a on:click={openBogota} href={'#'}>
 				<p class="normal-case text-xl font-medium font-neue-power tracking-wide">Bogotá</p>
@@ -299,7 +342,7 @@
 
 		<a
 			href={'#'}
-			class="relative lp:absolute bg-primary text-secondary lp:left-[40%] lp:top-[45%] sp:top-[33%] dp:top-[37%] dp:left-[40%] bg:top-[35%] bg:left-[42%] 2k:top-[35%] 2k:left-[41%] px-3 py-1 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
+			class="relative lp:absolute bg-primary text-secondary lp:left-[40%] lp:top-[37%] sp:top-[33%] dp:top-[33%] dp:left-[40%] bg:top-[33%] bg:left-[42%] 2k:top-[41%] 2k:left-[41%] px-3 py-1 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
 		>
 			<a on:click={openLisboa} href={'#'}>
 				<p class="normal-case text-xl font-medium font-neue-power tracking-wide">Lisbon</p>
@@ -320,7 +363,7 @@
 				</a>
 				<hr class="border-secondary" />
 				<a
-					on:click={() => pickEvent(4)}
+					on:click={() => pickEvent(8)}
 					href={'#'}
 					class="flex justify-between items-center py-2 gap-36"
 				>
@@ -336,7 +379,7 @@
 
 		<a
 			href={'#'}
-			class="relative lp:absolute bg-primary text-secondary lp:left-[45%] lp:top-[42%] sp:top-[28%] dp:top-[33%] dp:left-[45%] bg:top-[30%] bg:left-[45%] 2k:top-[30%] 2k:left-[45%] px-3 py-1 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
+			class="relative lp:absolute bg-primary text-secondary lp:left-[45%] lp:top-[30%] sp:top-[28%] dp:top-[27%] dp:left-[45%] bg:top-[27%] bg:left-[45%] 2k:top-[38%] 2k:left-[45%] px-3 py-1 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
 		>
 			<a on:click={openParis} href={'#'}>
 				<p class="normal-case text-xl font-medium font-neue-power tracking-wide">Paris</p>
@@ -371,29 +414,6 @@
 			</div>
 		</a>
 		
-        <a
-			href={'#'}
-			class="relative lp:absolute bg-primary text-secondary lp:left-[27.5%] lp:top-[65%] sp:top-[52%] bg:top-[54%] bg:left-[30%] 2k:left-[28%] px-3 py-1.5 rounded-sm shadow-md gap-3 normal-case text-xl font-medium font-neue-power tracking-wide"
-		>
-			<a on:click={openSaoPaulo} href={'#'}>
-				<p class="normal-case text-xl font-medium font-neue-power tracking-wide">São Paulo</p>
-			</a>
-			<div class={`${isSaoPauloOpen ? 'block' : 'hidden'} h-full`}>
-				<hr class="border-secondary mt-2" />
-				<a
-					on:click={() => pickEvent(7)}
-					href={'#'}
-					class="flex justify-between items-center py-2 gap-36"
-				>
-					<p class="text-sm font-normal tracking-wide">O.R.B</p>
-					<img
-						loading="lazy"
-						src={`/events/events-pick-${estilos[index].secondary_media}.svg`}
-						alt="pick event"
-					/>
-				</a>
-			</div>
-		</a>
 	</div>
 
 	{#each events as { banner, what, where, info, address }, index (index)}
