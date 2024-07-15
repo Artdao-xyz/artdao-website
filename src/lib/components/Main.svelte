15876993f6a342d66fff5614158bf47e17b17b2f
Pedro Falco <fpfalco@gmail.com>
2024-07-15 19:35:51 -0300

    fix: fix h1 poisition in mobile version

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -48,7 +48,7 @@
 			<!-- svelte-ignore a11y-missing-content -->
 			<h1
 				bind:this={h1}
-				class="fixed lp:top-1/2 lp:-translate-y-1/2 w-5/6 h-fit lp:ml-16 font-neue-power inline-block text-5xl text-left font-medium tb:text-3xl lp:text-8xl dp:text-9xl uppercase leading-10 tracking-wider whitespace-pre-line truncate"
+				class="fixed  left-4 lp:top-1/2 lp:-translate-y-1/2 w-5/6 h-fit lp:ml-16 font-neue-power inline-block text-5xl text-left font-medium tb:text-3xl lp:text-8xl dp:text-9xl uppercase leading-10 tracking-wider lp:whitespace-pre-line lp:truncate"
 			></h1>
 		</section>
 	</main></button
