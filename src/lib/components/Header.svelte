100d3384e7f399b4c19b300b9811f110c62e95ce
Pedro Falco <fpfalco@gmail.com>
2024-07-21 18:26:20 -0300

    refactor: refactor index to indexStyle store

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,19 +5,9 @@
 	import { Menu, Close } from 'svelte-ionicons';
 	import iosInnerHeight from '$lib/utils/iosInnerHeight.js';
 	import Control from './Control.svelte';
-
-	let index = 0;
-	let header;
-
-	onMount(() => {
-		indexStyle.subscribe((value) => {
-			index = value;
-		});
-	});
 </script>
 
 <header
-	bind:this={header}
 	class="bg-background text-primary font-clash-display sticky top-0 left-0 max-w-screen p-4 lp:pt-6 lp:px-14 lp:pb-0 z-30 dp:max-w-[1440px] dp:mx-auto"
 >
 	<nav class="flex justify-between items-center mx-auto">
@@ -25,7 +15,7 @@
 			<a href="/"
 				><img
 					
-					src={`/artdao-logo-${estilos[index].primary_media}.svg`}
+					src={`/artdao-logo-${estilos[$indexStyle].primary_media}.svg`}
 					alt="Artdao Logo"
 				/></a
 			>
